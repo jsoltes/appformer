7509027200c971544d50d8bbe06ae807cbf60895
Juraj Soltes <jsoltes@redhat.com>
2018-01-05 18:01:29 +0100

    create initial (not necessarily runnable) version of the reproducer

--- uberfire-extensions/uberfire-commons-editor/uberfire-commons-editor-client/src/test/java/org/uberfire/ext/editor/commons/client/history/VersionRecordsAfterRenameAndSaveTest.java
+++ uberfire-extensions/uberfire-commons-editor/uberfire-commons-editor-client/src/test/java/org/uberfire/ext/editor/commons/client/history/VersionRecordsAfterRenameAndSaveTest.java
@@ -6,14 +6,22 @@ import org.jboss.errai.common.client.api.Caller;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.uberfire.backend.vfs.ObservablePath;
+import org.uberfire.client.workbench.type.ClientResourceType;
+import org.uberfire.ext.editor.commons.client.BaseEditor;
 import org.uberfire.ext.editor.commons.client.file.RestoreUtil;
 import org.uberfire.ext.editor.commons.client.file.popups.RestorePopUpPresenter;
 import org.uberfire.ext.editor.commons.client.history.event.VersionSelectedEvent;
+import org.uberfire.ext.editor.commons.client.menu.MenuItems;
 import org.uberfire.ext.editor.commons.version.VersionService;
+import org.uberfire.mvp.PlaceRequest;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class VersionRecordsAfterRenameAndSaveTest {
 
     private static VersionRecordManager versionRecordManager;
+    private static BaseEditor baseEditor;
 
     @Mock
     private static VersionMenuDropDownButton versionMenuDropDownButton;
@@ -28,9 +36,15 @@ public class VersionRecordsAfterRenameAndSaveTest {
     @Mock
     private static Caller<VersionService> versionServiceCaller;
 
+    @Mock
+    private ObservablePath observablePath;
+    @Mock
+    private PlaceRequest placeRequest;
+    @Mock
+    private ClientResourceType clientResourceType;
+
     @Before
     public void setup() {
-
         versionRecordManager = new VersionRecordManager(
                 versionMenuDropDownButton,
                 saveButton,
@@ -39,11 +53,29 @@ public class VersionRecordsAfterRenameAndSaveTest {
                 versionSelectedEvent,
                 versionServiceCaller
         );
+
+        baseEditor = new BaseEditor(versionRecordManager) {
+            @Override
+            protected void loadContent() {
+            }
+        };
     }
 
     @Test
-    public void testGetCurrentVersionIndex() {
-        //call baseEditor.init(); then call baseEditor.onRename()
-        //assert that the versionHistory has changed to contain the new commit
+    public void testReproduceAF497() {
+        baseEditor.init(observablePath,
+                        placeRequest,
+                        clientResourceType,
+                        true,
+                        true,
+                        MenuItems.SAVE,
+                        MenuItems.DELETE,
+                        MenuItems.RENAME,
+                        MenuItems.COPY,
+                        MenuItems.HISTORY);
+        baseEditor.onRename();
+        baseEditor.onSave();
+        //after rename, the versionRecordManager is not refreshed, so on save the the version history gets out of the index
+        assertThat("-1").isNotEqualTo(versionRecordManager.getVersion());
     }
 }
